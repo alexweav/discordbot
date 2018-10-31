571563774c5ac89f960b95724800de4781687259
alexweav <alexander.weaver@ttu.edu>
2018-10-30 21:38:59 -0500

    Rename and setup application

--- lib/discordbot.ex
+++ lib/discordbot.ex
@@ -1,10 +1,14 @@
 defmodule DiscordBot.Application do
+  require Logger
   use Application
+
   @moduledoc """
   Entry point for the bot
   """
 
   def start(_type, _args) do
-
+    children = []
+    Logger.info("Launching...")
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
--- mix.exs
+++ mix.exs
@@ -13,7 +13,8 @@ defmodule DiscordBot.MixProject do
 
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {DiscordBot, []}
     ]
   end
 
