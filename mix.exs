738e48b79c892589af9dd26906987ad5ca4278b1
alexweav <alexander.weaver@ttu.edu>
2018-10-31 00:04:08 -0500

    Change entry point to DiscordBot.Application and create top level application supervisor

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule DiscordBot.MixProject do
   def application do
     [
       extra_applications: [:logger],
-      mod: {DiscordBot, []}
+      mod: {DiscordBot.Application, []}
     ]
   end
 
