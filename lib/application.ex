79253223b59b2d1d4bd62de0e9c88c0247e64add
alexweav <alexander.weaver@ttu.edu>
2018-10-30 21:29:44 -0500

    Create aplication

---
+++ lib/application.ex
@@ -0,0 +1,10 @@
+defmodule DiscordBot.Application do
+  use Application
+  @moduledoc """
+  Entry point for the bot
+  """
+
+  def start(_type, _args) do
+
+  end
+end
--- lib/discordbot.ex
+++
@@ -1,4 +0,0 @@
-defmodule DiscordBot do
-  @moduledoc """
-  """
-end
