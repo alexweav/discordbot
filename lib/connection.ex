87ede9298280dae2db3c39543d026261a0abd43d
alexweav <alexander.weaver@ttu.edu>
2018-11-03 12:03:04 -0500

    Successfully connect to websocket

--- lib/connection.ex
+++ lib/connection.ex
@@ -4,8 +4,29 @@ defmodule DiscordBot.Connection do
   """
 
   use WebSockex
+  require Logger
 
   def start_link(url) do
-    WebSockex.start_link(url, __MODULE__, :ok)
+    WebSockex.start_link(url <> "/?v=6&encoding=json", __MODULE__, :ok)
+  end
+
+  def handle_connect(connection, _state) do
+    Logger.info("Connected!")
+    IO.inspect connection
+    {:ok, :ok}
+  end
+
+  def handle_frame(frame, _state) do
+    Logger.info("Got message.")
+    IO.inspect frame
+  end
+
+  def handle_disconnect(_reason, _state) do
+    Logger.info("Disconnected.")
+  end
+
+  def terminate(_reason, _state) do
+    Logger.info("Terminated.")
+    exit(:normal)
   end
 end
