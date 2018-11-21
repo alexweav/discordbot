09c5733d5356ad4103eb3215db413b6099de9f45
alexweav <alexander.weaver@ttu.edu>
2018-11-21 17:26:55 -0600

    Store token in distillery runtime config instead of basic mix config

---
+++ rel/config/config.sample.exs
@@ -0,0 +1,9 @@
+use Mix.Config
+
+# SAMPLE configuration file
+
+config :discordbot,
+  # Configures the application to use the Discord token provided below
+  # To configure the bot yourself, provide a valid token below
+  # Then, rename this file to "config.exs"
+  token: "MY_BOT_TOKEN"
