6a7ebac6712f81897d5c4a7c7c7e46f1f7ff94d7
alexweav <alexander.weaver@ttu.edu>
2018-12-18 18:55:28 -0600

    Create supervisor for channel processes

---
+++ lib/discordbot/channel/supervisor.ex
@@ -0,0 +1,19 @@
+defmodule DiscordBot.Channel.Supervisor do
+  @moduledoc """
+  Supervisor for the Channel-related processes
+  """
+
+  use Supervisor
+
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, :ok, opts)
+  end
+
+  def init(:ok) do
+    children = [
+      {DynamicSupervisor, name: DiscordBot.ChannelSupervisor, strategy: :one_for_one}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_all)
+  end
+end
