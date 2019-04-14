789d71a9033183175ab91bc2efd4422cfc7e09a3
alexweav <weaver.alex.d@gmail.com>
2019-04-14 14:34:53 -0500

    Add tests for authenticator

--- apps/discordbot/lib/discordbot/gateway/authenticator.ex
+++ apps/discordbot/lib/discordbot/gateway/authenticator.ex
@@ -14,19 +14,19 @@ defmodule DiscordBot.Gateway.Authenticator do
   to authenticate the event's source connection with `token`.
   """
   @spec authenticate(pid | atom, String.t()) :: :ok
-  def authenticate(broker, token), do: authenticate_shard(broker, token, 0, 1)
+  def authenticate(broker, token), do: authenticate(broker, token, 0, 1)
 
   @doc """
   Authenticates connections and provides sharding info.
   """
-  @spec authenticate_shard(pid | atom, String.t(), integer, integer) :: :ok
-  def authenticate_shard(broker, token, shard_index, shard_count) do
+  @spec authenticate(pid | atom, String.t(), integer, integer) :: :ok
+  def authenticate(broker, token, shard_index, shard_count) do
     Broker.subscribe(broker, :hello)
-    connection = wait_for_connect(broker)
+    connection = wait_for_connect()
     DiscordBot.Gateway.Connection.identify(connection, token, shard_index, shard_count)
   end
 
-  defp wait_for_connect(_broker) do
+  defp wait_for_connect() do
     receive do
       %Event{publisher: publisher} -> publisher
     end
---
+++ apps/discordbot/test/gateway/authenticator_test.exs
@@ -0,0 +1,47 @@
+defmodule DiscordBot.Gateway.AuthenticatorTest do
+  use ExUnit.Case, async: true
+
+  alias DiscordBot.Gateway.Authenticator
+  alias DiscordBot.Broker
+
+  setup context do
+    broker = start_supervised!({Broker, []})
+    %{broker: broker, context: context}
+  end
+
+  test "subscribes to :hello events on launch", %{broker: broker, context: context} do
+    auth =
+      start_supervised!(
+        {Task, fn -> Authenticator.authenticate(broker, "test") end},
+        id: context.test
+      )
+
+    Process.sleep(10)
+
+    assert broker
+           |> Broker.subscribers?(:hello)
+           |> Enum.member?(auth)
+  end
+
+  test "publishing :hello event triggers authentication", %{broker: broker, context: context} do
+    start_supervised!(
+      {Task, fn -> Authenticator.authenticate(broker, "test") end},
+      id: context.test
+    )
+
+    Process.sleep(10)
+    Broker.publish(broker, :hello, {})
+    assert_receive({:"$websockex_cast", {:identify, "test", 0, 1}}, 1_000)
+  end
+
+  test ":hello with custom sharding authenticates", %{broker: broker, context: context} do
+    start_supervised!(
+      {Task, fn -> Authenticator.authenticate(broker, "asdf", 31, 128) end},
+      id: context.test
+    )
+
+    Process.sleep(10)
+    Broker.publish(broker, :hello, {})
+    assert_receive({:"$websockex_cast", {:identify, "asdf", 31, 128}}, 1_000)
+  end
+end
