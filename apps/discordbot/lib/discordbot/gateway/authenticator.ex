26bec5945675e8cdb227cc09f0ab3fa27b5bb48c
alexweav <weaver.alex.d@gmail.com>
2019-04-14 14:02:09 -0500

    Sharding controls for authenticator

--- apps/discordbot/lib/discordbot/gateway/authenticator.ex
+++ apps/discordbot/lib/discordbot/gateway/authenticator.ex
@@ -14,11 +14,16 @@ defmodule DiscordBot.Gateway.Authenticator do
   to authenticate the event's source connection with `token`.
   """
   @spec authenticate(pid | atom, String.t()) :: :ok
-  def authenticate(broker, token) do
+  def authenticate(broker, token), do: authenticate_shard(broker, token, 0, 1)
+
+  @doc """
+  Authenticates connections and provides sharding info.
+  """
+  @spec authenticate_shard(pid | atom, String.t(), integer, integer) :: :ok
+  def authenticate_shard(broker, token, shard_index, shard_count) do
     Broker.subscribe(broker, :hello)
     connection = wait_for_connect(broker)
-    DiscordBot.Gateway.Connection.identify(connection, token, 0, 1)
-    :ok
+    DiscordBot.Gateway.Connection.identify(connection, token, shard_index, shard_count)
   end
 
   defp wait_for_connect(_broker) do
