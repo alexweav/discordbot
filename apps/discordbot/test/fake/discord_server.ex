ebecdc8ca796e8416b9821207c10c5c088a21e06
alexweav <weaver.alex.d@gmail.com>
2019-04-21 19:17:15 -0500

    Shutdown test server when tests are complete

--- apps/discordbot/test/fake/discord_server.ex
+++ apps/discordbot/test/fake/discord_server.ex
@@ -5,12 +5,16 @@ defmodule DiscordBot.Fake.DiscordServer do
   plug(:match)
   plug(:dispatch)
 
-  def start(_pid) do
+  def start() do
     ref = make_ref()
     url = "wss://localhost:#{8473}/fakecord"
     opts = [port: 8473, ref: ref]
     Plug.Adapters.Cowboy.http(__MODULE__, [], opts)
     {:ok, {url, ref}}
+  end
+
+  def shutdown(ref) do
+    Plug.Adapters.Cowboy.shutdown(ref)
   end
 
   match _ do
--- apps/discordbot/test/gateway/connection_test.exs
+++ apps/discordbot/test/gateway/connection_test.exs
@@ -2,7 +2,12 @@ defmodule DiscordBot.Gateway.ConnectionTest do
   use ExUnit.Case, async: true
 
   setup do
-    {:ok, {url, ref}} = DiscordBot.Fake.DiscordServer.start(self())
+    {:ok, {url, ref}} = DiscordBot.Fake.DiscordServer.start()
+
+    on_exit(fn ->
+      DiscordBot.Fake.DiscordServer.shutdown(ref)
+    end)
+
     %{url: url, ref: ref}
   end
 
