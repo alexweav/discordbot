2b4f42a81b0f36022a61437b2f1e7c8d6c6f2510
alexweav <weaver.alex.d@gmail.com>
2019-04-21 20:18:19 -0500

    Spin up broker for connection tests

--- apps/discordbot/test/gateway/connection_test.exs
+++ apps/discordbot/test/gateway/connection_test.exs
@@ -1,18 +1,23 @@
 defmodule DiscordBot.Gateway.ConnectionTest do
   use ExUnit.Case, async: true
 
-  setup do
+  alias DiscordBot.Broker
+  alias DiscordBot.Gateway.Connection
+
+  setup context do
     {:ok, {url, ref}} = DiscordBot.Fake.DiscordServer.start()
 
     on_exit(fn ->
       DiscordBot.Fake.DiscordServer.shutdown(ref)
     end)
 
-    %{url: url, ref: ref}
+    broker = start_supervised!({Broker, []}, id: Module.concat(context.test, :broker))
+
+    %{url: url, ref: ref, broker: broker, test: context.test}
   end
 
-  test "establishes websocket connection using URL", %{url: url} do
-    pid = start_supervised!({DiscordBot.Gateway.Connection, token: "asdf", url: url})
-    assert DiscordBot.Gateway.Connection.disconnect(pid, 4001) == :ok
+  test "establishes websocket connection using URL", %{url: url, broker: broker, test: test} do
+    pid = start_supervised!({Connection, token: "asdf", url: url, broker: broker}, id: test)
+    assert Connection.disconnect(pid, 4001) == :ok
   end
 end
