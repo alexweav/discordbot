786f80c00ee79d231a41c251661de5b64e8c03a6
alexweav <weaver.alex.d@gmail.com>
2019-05-13 00:17:17 -0500

    Add a supervisor for individual voice sessions

--- apps/discordbot/lib/discordbot/voice/session.ex
+++ apps/discordbot/lib/discordbot/voice/session.ex
@@ -5,16 +5,39 @@ defmodule DiscordBot.Voice.Session do
 
   use Supervisor
 
+  alias DiscordBot.Util
+
   @doc """
   Starts a voice session.
   """
   def start_link(opts) do
-    Supervisor.start_link(__MODULE__, :ok, opts)
+    url = Util.require_opt!(opts, :url)
+    server_id = Util.require_opt!(opts, :server_id)
+    user_id = Util.require_opt!(opts, :user_id)
+    session_id = Util.require_opt!(opts, :session_id)
+    token = Util.require_opt!(opts, :token)
+
+    values = %{
+      server_id: server_id,
+      user_id: user_id,
+      session_id: session_id,
+      token: token,
+      url: url
+    }
+
+    Supervisor.start_link(__MODULE__, values, opts)
   end
 
   @doc false
-  def init(:ok) do
-    children = []
+  def init(values) do
+    children = [
+      {DiscordBot.Voice.Control,
+       url: values[:url],
+       server_id: values[:server_id],
+       user_id: values[:user_id],
+       session_id: values[:session_id],
+       token: values[:token]}
+    ]
 
     Supervisor.init(children, strategy: :one_for_all)
   end
