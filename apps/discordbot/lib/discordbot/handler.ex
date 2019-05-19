fbb5fd5bcf141eae29a9ff0104ac5fddf0d4ae0b
alexweav <weaver.alex.d@gmail.com>
2019-05-18 20:03:07 -0500

    Add start_link and start to handler

--- apps/discordbot/lib/discordbot/handler.ex
+++ apps/discordbot/lib/discordbot/handler.ex
@@ -22,4 +22,20 @@ defmodule DiscordBot.Handler do
   @callback handle_event(event :: Event.t(), state :: term) ::
               {:ok, new_state}
             when new_state: term
+
+  @doc """
+  Starts a Handler process linked to the current process.
+  """
+  @spec start_link(atom, any, list) :: GenServer.on_start()
+  def start_link(module, init_arg, options \\ []) when is_atom(module) and is_list(options) do
+    GenServer.start_link(module, init_arg, options)
+  end
+
+  @doc """
+  Starts a Handler process without links (outside of a supervision tree).
+  """
+  @spec start(atom, any, list) :: GenServer.on_start()
+  def start(module, init_arg, options \\ []) when is_atom(module) and is_list(options) do
+    GenServer.start(module, init_arg, options)
+  end
 end
