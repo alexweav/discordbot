2542fb3a8633217c8ee97060b3f49a955c8deb0a
alexweav <weaver.alex.d@gmail.com>
2019-05-19 20:38:34 -0500

    Tests for help info and subscriptions for Ping

--- apps/services/test/services/ping_test.exs
+++ apps/services/test/services/ping_test.exs
@@ -2,7 +2,23 @@ defmodule Services.PingTest do
   use ExUnit.Case, async: true
   doctest Services.Ping
 
-  alias Services.Ping
+  alias DiscordBot.Broker
+  alias Services.{Help, Ping}
+
+  setup context do
+    broker = start_supervised!({Broker, []}, id: Module.concat(context.test, :broker))
+
+    help =
+      start_supervised!({Help, [broker: broker, name: context.test]},
+        id: Module.concat(context.test, :help),
+        restart: :transient
+      )
+
+    ping =
+      start_supervised!({Ping, help: help, broker: broker}, id: Module.concat(context.test, :ping))
+
+    %{broker: broker, help: help, ping: ping}
+  end
 
   test "responds to ping query" do
     assert Ping.handle_message("!ping", :ok) == {:reply, {:text, "Pong!"}}
@@ -17,5 +33,14 @@ defmodule Services.PingTest do
     assert Ping.handle_message("!Ping", :ok) == {:noreply}
     assert Ping.handle_message("asdfasdf", :ok) == {:noreply}
     assert Ping.handle_message("", :ok) == {:noreply}
+  end
+
+  test "registers help info on start", %{help: help} do
+    assert {:ok, _ping_info} = Help.info?(help, "!ping")
+    assert {:ok, _src_info} = Help.info?(help, "!source")
+  end
+
+  test "subscribes to messages on start", %{broker: broker, ping: ping} do
+    assert Enum.member?(Broker.subscribers?(broker, :message_create), ping)
   end
 end
