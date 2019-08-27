7d15c5ffa9ab397a1130df09aa0aee57a5239dea
Alex Weaver <weaver.alex.d@gmail.com>
2019-08-27 00:05:05 -0500

    Collateral from upgrading distillery

--- rel/config.exs
+++ rel/config.exs
@@ -3,7 +3,7 @@
 |> Path.wildcard()
 |> Enum.map(&Code.eval_file(&1))
 
-use Mix.Releases.Config,
+use Distillery.Releases.Config,
     default_release: :default,
     default_environment: Mix.env()
 
