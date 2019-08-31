833fc902caf0cc4257bac97d97c9f1b33bf5fe00
Alex Weaver <weaver.alex.d@gmail.com>
2019-08-31 00:26:17 -0500

    String arg insert tests

--- apps/services/test/services/auto_responder_test.exs
+++ apps/services/test/services/auto_responder_test.exs
@@ -1,4 +1,18 @@
 defmodule Services.AutoResponderTest do
   use ExUnit.Case, async: true
   doctest Services.AutoResponder
+
+  alias Services.AutoResponder
+
+  test "inserts args into strings" do
+    string = "A {arg}"
+    args = %{"foo" => "bar", "arg" => "test"}
+    assert AutoResponder.insert_string_args(string, args) == "A test"
+  end
+
+  test "leaves non-matching args alone" do
+    string = "A {arg} and {another}"
+    args = %{"foo" => "bar", "arg" => "test"}
+    assert AutoResponder.insert_string_args(string, args) == "A test and {another}"
+  end
 end
