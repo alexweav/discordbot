78e957afb311808594ee8f85e71a6953799e9532
Alex Weaver <weaver.alex.d@gmail.com>
2019-08-31 17:17:44 -0500

    Validate POST body

--- apps/services/test/fake/spotify/router.ex
+++ apps/services/test/fake/spotify/router.ex
@@ -17,13 +17,22 @@ defmodule Services.Fake.Spotify.Router do
   plug(:dispatch)
 
   post "/api/token" do
-    conn
-    |> put_resp_content_type("application/json")
-    |> send_resp(200, Poison.encode!(%{access_token: @fake_token}))
+    with :ok <- validate_token_request_body(conn.body_params) do
+      conn
+      |> put_resp_content_type("application/json")
+      |> send_resp(200, Poison.encode!(%{access_token: @fake_token}))
+    else
+      error ->
+        conn
+        |> send_resp(400, "Request for token did not match: #{error}")
+    end
   end
 
   match _ do
     conn
     |> send_resp(404, "Oops")
   end
+
+  defp validate_token_request_body(%{"grant_type" => "client_credentials"}), do: :ok
+  defp validate_token_request_body(body), do: {:error, "Invalid POST body: #{body}"}
 end
