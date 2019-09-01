ded8309c55656fcc35da366b4ad462b6003287c9
Alex Weaver <weaver.alex.d@gmail.com>
2019-09-01 00:32:42 -0500

    Use new mechanism for TOKEN env var

--- apps/discordbot/lib/discordbot/configuration.ex
+++ apps/discordbot/lib/discordbot/configuration.ex
@@ -11,32 +11,17 @@ defmodule DiscordBot.Configuration do
   """
   @spec token!() :: String.t()
   def token! do
-    with nil <- token_env(),
-         nil <- token_config() do
-      raise(
-        "No token found. Please provide a bot token, either by environment variable (TOKEN) or via confix.exs."
-      )
-    else
-      token -> token
+    load_env_var(@token_env_var_key, :discordbot, :token)
+
+    case Application.fetch_env(:discordbot, :token) do
+      {:ok, token} ->
+        token
+
+      :error ->
+        raise(
+          "No token found. Please provide a bot token, either by environment variable (TOKEN) or via confix.exs."
+        )
     end
-  end
-
-  @doc """
-  Returns the discord bot token, if it is defined via environment variable,
-  or `nil` otherwise.
-  """
-  @spec token_env() :: String.t() | nil
-  def token_env do
-    Map.get(System.get_env(), @token_env_var_key)
-  end
-
-  @doc """
-  Returns the discord bot token, if it is defined via application configuration,
-  or `nil` otherwise.
-  """
-  @spec token_config() :: String.t() | nil
-  def token_config do
-    Application.get_env(:discordbot, :token)
   end
 
   @doc """
