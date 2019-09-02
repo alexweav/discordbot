4f555afeebe5fc8b7de76512d607936148a3bde5
Alex Weaver <weaver.alex.d@gmail.com>
2019-09-02 14:44:39 -0500

    Create mention responder

---
+++ apps/services/lib/services/mention.ex
@@ -0,0 +1,38 @@
+defmodule Services.Mention do
+  @moduledoc """
+  Responds to bot mentions.
+  """
+
+  use DiscordBot.Handler
+
+  alias DiscordBot.Model.Message
+  alias DiscordBot.Model.User
+  alias DiscordBot.Self
+
+  @doc """
+  Starts the handler.
+  """
+  def start_link(opts) do
+    DiscordBot.Handler.start_link(__MODULE__, :message_create, :ok, opts)
+  end
+
+  @doc false
+  def handler_init(_) do
+    {:ok, :ok}
+  end
+
+  @doc false
+  def handle_message(_, %Message{mentions: mentions}, _) do
+    %User{id: self_id} = Self.user?()
+
+    IO.inspect self_id
+    IO.inspect mentions
+    if Enum.any?(mentions, fn user -> user.id == self_id end) do
+      {:reply, {:text, "can you dont"}}
+    else
+      {:noreply}
+    end
+  end
+
+  def handle_message(_, _, _), do: {:noreply}
+end
