309c5a393eb1715eccb9da59205fb492e3d6e80e
Alex Weaver <weaver.alex.d@gmail.com>
2019-09-17 22:01:45 -0500

    Tests for remaining user properties

--- apps/discordbot/test/discordbot/self_test.exs
+++ apps/discordbot/test/discordbot/self_test.exs
@@ -3,6 +3,7 @@ defmodule DiscordBot.SelfTest do
   doctest DiscordBot.Self
 
   alias DiscordBot.Broker
+  alias DiscordBot.Model.{Ready, User}
   alias DiscordBot.Self
 
   setup context do
@@ -18,5 +19,51 @@ defmodule DiscordBot.SelfTest do
   test "uninitialized on launch", %{broker: broker} do
     Self.start_link(broker: broker, name: DiscordBot.Self)
     assert Self.status?() == :uninitialized
+  end
+
+  test "initialized after ready", %{broker: broker} do
+    Self.start_link(broker: broker, name: DiscordBot.Self)
+    Broker.publish(broker, :ready, test_ready())
+    assert Self.status?() == :initialized
+  end
+
+  test "retrieves full user struct", %{broker: broker} do
+    Self.start_link(broker: broker, name: DiscordBot.Self)
+    Broker.publish(broker, :ready, test_ready())
+    assert Self.user?() == test_ready().user
+  end
+
+  test "retrieves bot username", %{broker: broker} do
+    Self.start_link(broker: broker, name: DiscordBot.Self)
+    Broker.publish(broker, :ready, test_ready())
+    assert Self.username?() == "a-bot"
+  end
+
+  test "retrieves bot discriminator", %{broker: broker} do
+    Self.start_link(broker: broker, name: DiscordBot.Self)
+    Broker.publish(broker, :ready, test_ready())
+    assert Self.discriminator?() == "1234"
+  end
+
+  test "retrieves bot ID", %{broker: broker} do
+    Self.start_link(broker: broker, name: DiscordBot.Self)
+    Broker.publish(broker, :ready, test_ready())
+    assert Self.id?() == "123456789"
+  end
+
+  defp test_ready do
+    %Ready{
+      v: 6,
+      user: %User{
+        id: "123456789",
+        username: "a-bot",
+        discriminator: "1234",
+        bot: true
+      },
+      private_channels: [],
+      guilds: [],
+      session_id: "asdf",
+      _trace: "test guild"
+    }
   end
 end
