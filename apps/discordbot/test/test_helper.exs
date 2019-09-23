da2caee144abb4074c151fdc85688190bcc3484b
Alex Weaver <weaver.alex.d@gmail.com>
2019-09-22 20:59:06 -0500

    Remove old channel cache implementation

--- apps/discordbot/test/test_helper.exs
+++ apps/discordbot/test/test_helper.exs
@@ -3,7 +3,6 @@ Application.ensure_all_started(:cowboy)
 
 Mox.defmock(DiscordBot.ApiMock, for: DiscordBot.Api)
 Mox.defmock(DiscordBot.MessagesMock, for: DiscordBot.Entity.Messages)
-Mox.defmock(DiscordBot.Entity.ChannelManagerMock, for: DiscordBot.Entity.ChannelManager)
 
 Application.put_env(:discordbot, :http_api, DiscordBot.ApiMock)
 
