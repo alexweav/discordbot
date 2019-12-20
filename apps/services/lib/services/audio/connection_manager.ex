be03160304713013364b7bfe45d24fe05171604b
Alex Weaver <weaver.alex.d@gmail.com>
2019-12-19 18:30:36 -0600

    Create channel

--- apps/services/lib/services/audio/connection_manager.ex
+++ apps/services/lib/services/audio/connection_manager.ex
@@ -7,29 +7,34 @@ defmodule Services.Audio.ConnectionManager do
 
   require Logger
 
-  # @rmq_hostname "rabbitmq"
-  # @rmq_username "guest"
-  # @rmq_password "guest"
+  @rmq_hostname "rabbitmq"
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, :ok, opts)
   end
 
-  def open(manager, host) do
-    GenServer.cast(manager, {:connect, host})
+  def get_channel!(manager) do
+    GenServer.call(manager, :get_channel)
   end
 
   ## Handlers
 
   def init(:ok) do
-    {:ok, :ok}
+    {:ok, nil}
   end
 
-  def handle_cast({:connect, host}, state) do
-    Logger.info("Attempting to connect to #{host}")
-    {:ok, connection} = AMQP.Connection.open(host: host)
-    IO.inspect(connection)
-    Logger.info("Connection succeeded")
-    {:noreply, state}
+  def handle_call(:get_channel, _from, connection) do
+    connection = try_connect!(connection)
+    {:ok, channel} = AMQP.Channel.open(connection)
+    {:reply, channel, connection}
   end
+
+  defp try_connect!(nil) do
+    Logger.info("Attempting to connect to RabbitMQ at #{@rmq_hostname}")
+    {:ok, connection} = AMQP.Connection.open(host: @rmq_hostname)
+    Logger.info("Connection succeeded")
+    connection
+  end
+
+  defp try_connect!(connection), do: connection
 end
