b4831cd0158323c6eb19a9565602bce4859ff717
Alex Weaver <weaver.alex.d@gmail.com>
2020-06-14 23:22:08 -0500

    Refactor voice streaming logic, better error handling

--- apps/services/lib/services/audio/downloader.ex
+++ apps/services/lib/services/audio/downloader.ex
@@ -28,8 +28,8 @@ defmodule Services.Audio.Downloader do
     uri = "/files/" <> URI.encode(path)
 
     case Downloader.get(uri) do
-      {:ok, %Response{status_code: 200, body: body} -> {:ok, body}
-      {:error, %Response{status_code: 404}} -> {:error, :notfound}
+      {:ok, %Response{status_code: 200, body: body}} -> {:ok, body}
+      {:ok, %Response{status_code: 404}} -> {:error, :notfound}
       {:error, response} -> {:error, response}
     end
   end
@@ -42,7 +42,6 @@ defmodule Services.Audio.Downloader do
     uri = "/files/" <> URI.encode(path) <> "/content"
     url = process_request_url(uri)
 
-    {:ok, :saved_to_file} =
     result = :httpc.request(:get, {to_charlist(url), []}, [], stream: to_charlist(local_path))
 
     case result do
